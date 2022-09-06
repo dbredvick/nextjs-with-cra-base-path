fcfd14178c4346417f7f7cdbc8e77ba5ffb5201e
Drew Bredvick <dbredvick@gmail.com>
2022-09-06 12:30:52 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+   async rewrites() {
+    return [
+      {
+        source: '/react',
+        destination: 'https://create-react-app-base-path.vercel.zone/',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
